3f593219ddb4ef1683bc3f2ec3bf1b76aeb470b6
f1rA5h <shtey.alexey@gmail.com>
2021-08-25 11:24:56 +0300

    minor

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Jam Academy',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
